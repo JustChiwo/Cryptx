5999c089a5eb5c134746b7e0744faff4fb528e0d
Just.Chiwo <chiwomvundura@gmail.com>
2026-09-21 18:31:32 +0200

    Fixed layout of header and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Sidebar from "./components/layout/Sidebar";
 import Header from "./components/layout/Header";
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="flex h-screen">
       <Sidebar />
+      <div className="flex-1 flex flex-col overflow-y-auto">
+      <Header />
+    </div>
     </div>
   );
 }
